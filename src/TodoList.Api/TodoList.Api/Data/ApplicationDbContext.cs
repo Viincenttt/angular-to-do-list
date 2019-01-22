629d82b9bf7d1c6d92615fc40bafd5d0b576741d
Vincent <vincentkok@live.nl>
2019-01-22 15:29:13 +0100

    Added EntityFramework and a DbContext to work with

---
+++ src/TodoList.Api/TodoList.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace TodoList.Api.Data {
+    public class ApplicationDbContext : DbContext {
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
