679bda74e878f8f096561d4fa64fe7314df9a5d7
Vincent <vincentkok@live.nl>
2019-01-25 10:41:42 +0100

    Added EF TestSetup class to make automated tests that depend on EF easier

--- src/TodoList.Api/TodoList.Tests/Data/ApplicationDbContextTests.cs
+++ src/TodoList.Api/TodoList.Tests/Data/ApplicationDbContextTests.cs
@@ -1,49 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Moq;
-using TodoList.Api.Data;
 using TodoList.Api.Data.Models;
-using TodoList.Api.Services;
+using TodoList.Tests.TestFramework;
 using Xunit;
 
 namespace TodoList.Tests.Data {
-    public class ApplicationDbContextTests {
+    public class ApplicationDbContextTests : EntityFrameworkTest {
         [Fact]
         public async void OnModelCreating_ShouldFilterTodoItemsOnApplicationUser_WhenPerformingAnyQuery() {
-            // Given: A in memory database context
-            var userIdProvider = new Mock<IUserIdProvider>();
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(databaseName: "TodoList")
-                .Options;
+            // Given: A in memory database context with two users - current user and other user
+            ApplicationUser otherUser = new ApplicationUser();
+            this.DbContext.ApplicationUsers.Add(otherUser);
 
-            using (var context = new ApplicationDbContext(options, userIdProvider.Object)) {
-                // When: Creating two users, each having a single todo item
-                ApplicationUser currentUser = new ApplicationUser();
-                ApplicationUser otherUser = new ApplicationUser();
-                context.ApplicationUsers.Add(currentUser);
-                context.ApplicationUsers.Add(otherUser);
-                context.SaveChanges();
-
-                userIdProvider.Setup(x => x.GetUserId()).Returns(currentUser.Id);
-                
-                context.TodoItems.Add(new TodoItem() {
-                    ApplicationUser = currentUser,
-                    Title = "Create more integration tests",
-                    Description = "But always have more unit tests",
-                    SortOrder = 0          
-                });
-
-                context.TodoItems.Add(new TodoItem() {
-                    ApplicationUser = otherUser,
-                    Title = "Bake cookies",
-                    Description = "",
-                    SortOrder = 1
-                });
-
-                context.SaveChanges();
-
-                // Then: The TodoItems DbSet should only return the item that belong to the current user
-                Assert.Equal(1, await context.TodoItems.CountAsync());
-            }
+            // When: We add another user and give both users a single todo item
+            this.DbContext.TodoItems.Add(new TodoItem() { ApplicationUser = this.CurrentUser });
+            this.DbContext.TodoItems.Add(new TodoItem() { ApplicationUser = otherUser });
+            this.DbContext.SaveChanges();
+            
+            // Then: The TodoItems DbSet should only return the item that belong to the current user
+            Assert.Equal(1, await this.DbContext.TodoItems.CountAsync());
         }
     }
 }
---
+++ src/TodoList.Api/TodoList.Tests/TestFramework/EntityFrameworkTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using TodoList.Api.Data;
+using TodoList.Api.Data.Models;
+using TodoList.Api.Services;
+
+namespace TodoList.Tests.TestFramework {
+    public abstract class EntityFrameworkTest : IDisposable {
+        private Mock<IUserIdProvider> _userIdProviderMock;
+        protected ApplicationUser CurrentUser { get; set; }
+        protected ApplicationDbContext DbContext { get; set; }
+
+        public EntityFrameworkTest() {
+            this.Initialize();
+        }
+
+        public void Dispose() {
+            this.DbContext.Dispose();
+        }
+
+        private void Initialize() {
+            this.SetupApplicationDbContext();
+            this.SeedCurrentUser();
+        }
+
+        private void SetupApplicationDbContext() {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TodoList")
+                .Options;
+            this._userIdProviderMock = new Mock<IUserIdProvider>();
+
+            this.DbContext = new ApplicationDbContext(options, this._userIdProviderMock.Object);
+        }
+
+        private void SeedCurrentUser() {
+            this.CurrentUser = new ApplicationUser();
+            this.DbContext.ApplicationUsers.Add(CurrentUser);
+            this.DbContext.SaveChanges();
+
+            this._userIdProviderMock.Setup(x => x.GetUserId()).Returns(this.CurrentUser.Id);
+        }
+    }
+}
