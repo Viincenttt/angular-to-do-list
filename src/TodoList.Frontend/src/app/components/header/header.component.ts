7a1b9287fa1d9b7cc58ae6ed27fcc954e862a1b5
Vincent <vincentkok@live.nl>
2019-02-01 15:22:07 +0100

    Added simple login form in header

---
+++ src/TodoList.Frontend/src/app/components/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.less']
+})
+export class HeaderComponent implements OnInit {
+  public loginForm: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.initLoginForm();
+  }
+
+  private initLoginForm(): void {
+    this.loginForm = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, Validators.required)
+    });
+  }
+
+  public onLogin(): void {
+    console.log(this.loginForm);
+  }
+}
