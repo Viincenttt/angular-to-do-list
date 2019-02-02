6873e0bb53150bd7f2ffeed13f61ba4c9d0bac68
Vincent <vincentkok@live.nl>
2019-02-02 21:43:33 +0100

    Added a edit form for todo items

--- src/TodoList.Frontend/src/app/components/todo/todo-edit/todo-edit.component.ts
+++ src/TodoList.Frontend/src/app/components/todo/todo-edit/todo-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TodoListService } from 'src/app/services/todolist.service';
 import { BehaviorSubject } from 'rxjs';
 import { TodoItemModel } from 'src/app/models/todoitem.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-edit',
@@ -9,19 +10,40 @@ import { TodoItemModel } from 'src/app/models/todoitem.model';
   styleUrls: ['./todo-edit.component.less']
 })
 export class TodoEditComponent implements OnInit, OnDestroy {
-  
+  public editForm: FormGroup;
   public idToEdit = new BehaviorSubject<number>(null);
   public editItem: TodoItemModel;
 
   constructor(private todoListService: TodoListService) { }
 
   ngOnInit() {
+    this.initEditForm();
+
     this.idToEdit.subscribe((id: number) => {
       this.editItem = this.todoListService.get(id);
+      this.editForm.setValue({
+        'title': this.editItem.title,
+        'description': this.editItem.description
+      });
     });
   }
 
   ngOnDestroy(): void {
     this.idToEdit.unsubscribe();
+  }
+
+  private initEditForm(): void {
+    this.editForm = new FormGroup({
+      'title': new FormControl(null, [Validators.required]),
+      'description': new FormControl(null, [Validators.required])
+    });
+  }
+
+  public onSave(): void {
+    if (this.editForm.invalid) {
+      return;
+    }
+
+    console.log(this.editForm.value);
   }
 }
